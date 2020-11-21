c2747ec145e8138797685c416e6a133b69dcbd16
green-gem <zsoetan@gmail.com>
2020-11-21 17:09:50 -0500

    created table from pdf :), need to make it a function for all the other years

--- import_data2.R
+++ import_data2.R
@@ -4,101 +4,84 @@ library(readr)
 library(tidyverse)
 library(stringr)
 
-# contra <- "Contra Costa"
-# delnorte
-# la
-# sanbenito
-# sanbern
-# sandiego
-# sanfran
-# sanjoaquin
-# sanluis
-# sanmateo
-# santabarb
-# santaclara 
-# santacruz <- "Santa Cruz"
-
 doc <- "Year 2014.pdf"
 txt <- pdf_text(doc)
 
-# seventeen <- cat(txt[17])
-# seventeen
+indices <- str_which(txt, "Table 1") 
+# only the first 2 references are relevant, as they point to the actual table
 
-a <- pdf_data(doc)[[17]] # page 17 is where the table is located
-#b <- txt %>% read_lines() 
-c <- txt[17] %>% 
+start_table1 <- txt[indices[1]] %>% 
+  read_lines() %>% # read in document line by line
+  str_squish() # get rid of extra whitespace
+
+end_table1 <- txt[indices[2]] %>% 
   read_lines() %>% 
   str_squish()
 
+start16 <- str_which(start_table1, "County Pounds Applied") # the last header for the table
+end16 <-  length(start_table1)
 
-z <- c[5:32] %>%
+start17 <- str_which(end_table1, "County Pounds Applied")
+end17 <-  length(end_table1)
+
+tbl1 <- start_table1[(start16+1):(end16-1)] %>% #get rid of the header and page number
+  str_replace_all(",", "") %>% # gets rid of commas in numbers
+  str_to_lower() %>% 
+  str_split(boundary("word")) # splits by whitespace
+
+tbl1_b <- end_table1[(start17+1):(end17-2)] %>% # get rid of the header, the total row, and page number
   str_replace_all(",", "") %>%
   str_to_lower() %>%
   str_split(boundary("word"))
 
 
-z  # need to get way to keep county names together
+# function to take in the list from before, combine county names, and create a single table
+combine_names <- function(listy) {
+  
+  #initialize empty data frame
+  df <- data.frame(matrix(NA, nrow = length(listy), ncol = 5))
+  # new column names, shorter than original header names
+  names(df) <- c("country", 
+                 "usage2013","rank2013", 
+                 "usage2014", "rank2014")
 
-#%>%   strsplit(split = " ")
-
-
-
-# sanluis <- z[[14]]
-# 
-# deed <- str_c(str_subset(sanluis, "[a-z]+"), sep = " 555")
-# 
-# 
-# c[1]
-# c[2]
-# c[3]
-# c[4]
-# c[5]
-# 
-# shasta <- c[23]
-
-#d <- sanluis %>% 
-  str_replace_all(",", "") %>%
-  str_to_lower() %>%
-  str_split("[0-9]")
+  # goes through each list in the list to create new row in data frame
+  for (r in 1:length(listy)) {
+    # county name was split in two (i.e. Los Angeles)
+    if (length(listy[[r]]) == 6) {
+      nim = paste(listy[[r]][1],
+                  listy[[r]][2])
+      df[r,1] <- nim
+      df[r,2] <- listy[[r]][3]
+      df[r,3] <- listy[[r]][4]
+      df[r,4] <- listy[[r]][5]
+      df[r,5] <- listy[[r]][6]
     
-    #boundary("word"))
-#"[a-z]\\s+[0-9]"
-#"\\s+[0-9]"
+    # county name was split in three (i.e. San Luis Obispo)
+    } else if (length(listy[[r]]) == 7) {
+      nim = paste(listy[[r]][1],
+                  listy[[r]][2],
+                  listy[[r]][3])
+
+      df[r,1] <- nim
+      df[r,2] <- listy[[r]][4]
+      df[r,3] <- listy[[r]][5]
+      df[r,4] <- listy[[r]][6]
+      df[r,5] <- listy[[r]][7]
+    # county name was one word, so it wasn't split (i.e. Alameda)
+      } else {
+      df[r,1] <- listy[[r]][1]
+      df[r,2] <- listy[[r]][2]
+      df[r,3] <- listy[[r]][3]
+      df[r,4] <- listy[[r]][4]
+      df[r,5] <- listy[[r]][5]
+    }
+  }
+  return(df)
+}
+
+# Combines the two dataframes into 1 to get a full table 1 :)
+tabel1 <- bind_rows(combine_names(tbl1),
+                   combine_names(tbl1_b))
 
 
-loc <- str_locate(sanluis, "[")
-head(print(txt))
-
-# wd <- getwd()
-# file_name <- paste0(wd, "/", "temp.txt")
-# write(txt, file = file_name, sep = "\r")
-# dat <- read.table(file_name, skip = 957, nrows = 1091-958)
-# 
-# lines <- strsplit(txt, '\r') %>% unlist
-# tbl2 <- which(grep1('Table 2', lines))[1]
-# tbl2
-# 
-# 
-# result <- txt %>% str_split("\n")
-# 
-# 
-# library(pdftools)
-# library(tidyverse)
-# 
-# str(txt)[[1]]
-
-# clean <- function(tabl){
-#   tabl <- str_split(tabl, "\r", simplify = T)
-#   
-#   
-#   tabl_start <- stringr::str_which(tabl, "Table 1:")
-#   tabl_end <- stringr::str_which(tabl, "Table 2")
-#   tabl <- tabl[1, (tabl_start +1 ):(tabl_end - 1)]
-#   #tabl <- str_replace_all(table, "\\s{2,}", "|")
-#   text_con <- textConnection(table)
-#   data_table <- read.csv(text_con, sep = "|")
-#   #colnames(data_table) <- c("Condition", "Males", "Females", "Total")
-#   #dplyr::mutate(data_table, Country = country_name)
-# }
-#   
-# clean(txt)
