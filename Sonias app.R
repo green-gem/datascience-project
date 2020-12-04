eed6397f725ed0a213d4b181b241266b37af4c4f
bnsonia <soyeonkim@hsph.harvard.edu>
2020-12-03 22:53:46 -0500

    Sonia's shiny app work

--- Sonias app.R
+++ Sonias app.R
@@ -13,6 +13,38 @@ library(tidyverse)
 library(shinyBS)
 library(RColorBrewer)
 library(shinydashboard)
+library(sp)
+library(rgeos)
+library(rgdal)
+library(maptools)
+library(leaflet)
+library(scales)
+#Creating Leaflet
+map <- readOGR(path.expand("cb_2018_us_county_20m.shp"),
+               layer = "cb_2018_us_county_20m", stringsAsFactors = FALSE)
+Statekey<-read.csv(paste0(getwd(),'/STATEFPtoSTATENAME_Key.csv'), colClasses=c('character'))
+map<-merge(x=map, y=Statekey, by="STATEFP", all=TRUE)
+SingleState <- subset(map, map$STATENAME %in% c(
+    "California"
+))
+
+lbwdata_2018 <- lbwdata %>% filter(Year == "2018") %>% mutate(Rate = Events/Total.Births*100)
+SingleState<-sp::merge(x=SingleState, y=lbwdata_2018, by.x="NAME", by.y="County", by=x)
+
+pal <- colorNumeric(
+    palette = "Spectral",
+    domain = SingleState$Rate, n=7)
+
+leafletmap <- leaflet(SingleState,options = leafletOptions(zoomControl = FALSE, zoomLevelFixed = FALSE, dragging=TRUE, minZoom = 3, maxZoom = 6)) %>% 
+    setView(lat = 36.778259, lng = -119.417931, zoom = 5) %>%
+    addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5, 
+                opacity = 1.0, fillOpacity = 0.5, layerId = ~NAME,
+                fillColor = ~pal(Rate),
+                highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                    bringToFront = TRUE), 
+                popup = ~as.factor(paste0("<b><font size=\"4\"><center>County: </b>",SingleState$NAME,"</font></center>","<b>% of Low Birth Weight Births: </b>", sprintf("%1.2f%%", SingleState$Rate),"<br/>"))) %>%
+    addLegend(pal = pal, values = SingleState$Rate, opacity = 1)
+
 
 #data wrangling for 5-year trend (2014-2018) 
 lbwdata<-read.csv(paste0(getwd(),"/low-and-very-low-birthweight-by-county-2014-2018 (1).csv"), header = TRUE, stringsAsFactors = FALSE)
@@ -20,6 +52,7 @@ lbwdata <- lbwdata %>% mutate(County = str_to_title(County))
 lbwdata$Events[is.na(lbwdata$Events)] <- 0
 lbwdata <- lbwdata %>% group_by(Year, County, Total.Births) %>% summarize(Events = sum(Events)) 
 lbwdata <- lbwdata %>% filter(!County == "california") 
+lbwdata <- lbwdata %>% mutate(Rate = Events/Total.Births)
 CaliforniaCounty <- map_data("county", "california")
 CaliforniaCounty <- CaliforniaCounty %>% mutate(subregion = str_to_title(subregion))
 head(CaliforniaCounty)
@@ -29,20 +62,43 @@ california_map <- california_map %>% mutate(County = str_to_title(County))
 #shinyapp 
 
 ui <- fluidPage(
-    theme=shinythemes::shinytheme("slate"),
-    titlePanel("Percent of Low Birth Weight (<2500g) in California"),
-    sidebarLayout(
-        sidebarPanel(
-            selectizeInput("countyInput", "Choose a County", choices = unique(california_map$County), selected = "Los Angeles", multiple = TRUE, options = list(maxItems = 6)),
-            sliderInput("yearInput", "Year", min = 2014, max = 2018, value = 2014, step = 1, 
-                                      sep = "", ticks = FALSE, animate = TRUE),
-           ), #closing sidebarpanel
-        mainPanel(plotOutput("lbwheatmap"),
-                  br(), br(),
-                  plotOutput("bargraph"), 
-                  verbatimTextOutput("rate")
-                 )
-        )#closing sidebarlayout
+    h3("Exploration of Pesticides and Perinatal Outcomes in California County"),
+    tabsetPanel(
+        tabPanel("2018 LBW data in California County", icon = icon("map-pin"),
+                 sidebarPanel(
+                     p(strong("Low Birth Weight"), "is defined as babies who are born weighing less than 2500g. They 
+                       are at greater risk of experiencing certain long-term complications later in life, including high blood pressure, diabetes, or 
+                       developmental delay."),
+                     br(),
+                     br(),
+                     strong("2018 Map Exploration"),
+                     p("On the top, we see a map of percentage of low birth weight (Year 2018) in the county of California. Please click on each county to see each county-specific percentage of low birth weight."), 
+                     br(),
+                     br(),
+                     br(),
+                     br(),
+                     strong("2014-2018 HeatMap Exploration"),
+                      p("The second map is a heatmap of low birth weight in the county of California from 2014-2018. Please click on the play button to see the trend in
+                        percentage of low birth weight across 5 years (2014-2018)."), 
+                     br(),
+                     br(),
+                     sliderInput("yearInput", "Year", min = 2014, max = 2018, value = 2014, step = 1, 
+                                 sep = "", ticks = FALSE, animate = TRUE)
+                       ), #closing sidebarpanel
+                 mainPanel(leafletOutput("leafletmap"),
+                           br(), br(),
+                           plotOutput("lbwheatmap"))),
+        tabPanel("California LBW Trend from 2014-2018", icon = icon("chart-bar"),
+                 sidebarPanel(
+                     selectizeInput("countyInput", "Choose a County", choices = unique(california_map$County), selected = "Los Angeles", multiple = TRUE, options = list(maxItems = 6)),
+                     sliderInput("yearInput", "Year", min = 2014, max = 2018, value = 2014, step = 1, 
+                                 sep = "", ticks = FALSE, animate = TRUE),
+                 ), #closing sidebarpanel 
+                mainPanel(plotOutput("bargraph"), 
+                               verbatimTextOutput("rate"), class = 'leftAlign'
+                 )#closing mainpanel
+                 )#closing tabpanel
+    )
 )#closing fluidpage
 
 server <- function(input, output) {
@@ -73,11 +129,26 @@ server <- function(input, output) {
     )
     
     b <- reactive({
-        california_map %>% 
+        lbwdata %>% 
             filter(Year %in% input$yearInput) %>% filter(County %in% input$countyInput)})
     output$rate <- renderPrint({
-        aggregate(Rate ~ County, data = b(), sum)
+        aggregate(Rate~County, data = b(), sum)
     })
+    
+ 
+    
+    output$leafletmap <- renderLeaflet({
+        leaflet(SingleState, options = leafletOptions(zoomControl = TRUE, zoomLevelFixed = FALSE, dragging=TRUE, minZoom = 5.3, maxZoom = 6)) %>% 
+            setView(lat = 36.778259, lng = -119.417931, zoom = 6) %>%
+            addPolygons(color = "Black", weight = 1, smoothFactor = 0.5, 
+                        opacity = 1.0, fillOpacity = 0.5, layerId = ~NAME,
+                        fillColor = ~pal(Rate), 
+                        popup = ~as.factor(paste0("<b><font size=\"4\"><center>County: </b>",SingleState$NAME,"</font></center>","<b>% of Low Birth Weight Births: </b>", sprintf("%1.2f%%", SingleState$Rate),"<br/>"))) %>%
+            addLegend(pal = pal, values = SingleState$Rate, opacity = 1, title="% Low Birth Weight")
+        
+    })
+        
+    
     
 
     
