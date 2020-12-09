55a33d648e6eeb88abfad663ac97aed325509eee
green-gem <zsoetan@gmail.com>
2020-12-09 09:09:28 -0500

    added more linear models

--- regression_code.R
+++ regression_code.R
@@ -27,7 +27,6 @@ mch_lbw_regression <- df2 %>%
   filter(Year == 2016) %>%
   mutate(agricultural = ifelse(County %in% agro, 1, 0))
 
-
 lbw_regression <- lbwdata %>% 
   filter(Year == 2016) %>%
   mutate(agricultural = ifelse(County %in% agro, 1, 0)) %>%
@@ -51,32 +50,24 @@ lbw_pt %>% ggplot(aes(rate_lbw, agricultural)) +
   geom_point()
 
 
-
-ptmod <- glm(agricultural ~ rate_pt, lbw_pt, family = binomial())
-summary(ptmod)                  
-
-lbwmod <- glm(agricultural ~ rate_lbw, lbw_pt, family = binomial())
-summary(lbwmod) 
-
-lbw_ptmod <- glm(agricultural ~ rate_pt + rate_lbw, lbw_pt, family = binomial())
-summary(lbw_ptmod)  
-
-lbw_ptmod2 <- glm(agricultural ~ rate_pt + rate_lbw + rate_pt*rate_lbw, lbw_pt, family = binomial())
-summary(lbw_ptmod2)  
+# 
+# ptmod <- glm(agricultural ~ rate_pt, lbw_pt, family = binomial())
+# summary(ptmod)                  
+# 
+# lbwmod <- glm(agricultural ~ rate_lbw, lbw_pt, family = binomial())
+# summary(lbwmod) 
+# 
+# lbw_ptmod <- glm(agricultural ~ rate_pt + rate_lbw, lbw_pt, family = binomial())
+# summary(lbw_ptmod)  
+# 
+# lbw_ptmod2 <- glm(agricultural ~ rate_pt + rate_lbw + rate_pt*rate_lbw, lbw_pt, family = binomial())
+# summary(lbw_ptmod2)  
 
  
 lbw_pt %>% ggplot(aes(rate_pt, agricultural)) + 
   geom_point() +
   geom_line(aes(y = predict(ptmod, type = "response")))
 
-
-df2 %>% mutate(agricultural = ifelse(County %in% agro, 1, 0)) %>%
-  ggplot(aes(Year, Average.Birth.Weight, color = County)) + 
-  geom_point() 
-
-df2 %>% mutate(agricultural = ifelse(County %in% agro, 1, 0)) %>%
-  ggplot(aes(Year, Average.Birth.Weight, color = factor(agricultural))) + 
-  geom_point()
 
 MCH.CDC.Data_Race %>% mutate(agricultural = ifelse(County %in% agro, 1, 0)) %>%
   ggplot(aes(Year, Average.Birth.Weight, color = Mothers.Race)) + 
@@ -87,37 +78,59 @@ MCH.CDC.Data_Race %>% mutate(agricultural = ifelse(County %in% agro, 1, 0)) %>%
   geom_point()
 
 
+mod1 <- lm(Average.Birth.Weight ~ factor(agricultural) + Average.LMP.Gestational.Age, mch_regression)
+summary(mod1)
+
+mod2 <- lm(Average.Birth.Weight ~ factor(agricultural) + Average.LMP.Gestational.Age + factor(agricultural)*Average.LMP.Gestational.Age, mch_regression)
+summary(mod2)
+
+lbw_lmp_age2 = (mch_regression$Average.LMP.Gestational.Age)^2
+mod3 <- lm(Average.Birth.Weight ~ factor(agricultural) + Average.LMP.Gestational.Age + lbw_lmp_age2, mch_regression)
+summary(mod3)
+
+mch_regression %>%
+  ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = factor(agricultural))) + 
+  geom_point() + geom_smooth(se = F)
+
+MCH.CDC.Data_Race %>% mutate(agricultural = ifelse(County %in% agro, 1, 0)) %>%
+  ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = factor(agricultural))) + 
+  geom_point() + geom_smooth(se = F)
+
+mch_regression %>%
+  ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = factor(agricultural))) + 
+  geom_point() + geom_line(aes(y = predict(mod1)))
+
+mch_regression %>%
+  ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = factor(agricultural))) + 
+  geom_point() + geom_line(aes(y = predict(mod2)))
+
+mch_regression %>%
+  ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = factor(agricultural))) + 
+  geom_point() + geom_line(aes(y = predict(mod3)))
+
+
+avg_bw_mod2016 <- lm(Average.Birth.Weight ~ factor(Mothers.Race, ordered = F) + Average.LMP.Gestational.Age, mch_regression)
+summary(avg_bw_mod2016)
 
 mch_regression %>%
   ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = Mothers.Race)) + 
   geom_point() + geom_line(aes(y = predict(avg_bw_mod2016)))
 
-
-mod1 <- lm(Average.Birth.Weight ~ agricultural, mch_regression)
-summary(mod1)
-
-avg_bw_mod2016 <- lm(Average.Birth.Weight ~ factor(Mothers.Race, ordered = F) + Average.LMP.Gestational.Age, mch_regression)
-summary(avg_bw_mod2016)
-
-avg_bw_mod2007 <- lm(Average.Birth.Weight ~ factor(Mothers.Race, ordered = F) + Average.LMP.Gestational.Age, filter(MCH.CDC.Data_Race, Year == 2007))
-summary(avg_bw_mod2007)
-avg_bw_mod2010 <- lm(Average.Birth.Weight ~ factor(Mothers.Race, ordered = F) + Average.LMP.Gestational.Age, filter(MCH.CDC.Data_Race, Year == 2010))
-summary(avg_bw_mod2010)
-avg_bw_mod2013 <- lm(Average.Birth.Weight ~ factor(Mothers.Race, ordered = F) + Average.LMP.Gestational.Age, filter(MCH.CDC.Data_Race, Year == 2013))
-summary(avg_bw_mod2013)
-avg_bw_mod2019 <- lm(Average.Birth.Weight ~ factor(Mothers.Race, ordered = F) + Average.LMP.Gestational.Age, filter(MCH.CDC.Data_Race, Year == 2019))
-summary(avg_bw_mod2019)
-
-
-MCH.CDC.Data_Race %>% filter(Year == 2013) %>%
+mch_regression %>%
   ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = Mothers.Race)) + 
-  geom_point() + geom_line(aes(y = predict(avg_bw_mod2013)))
-MCH.CDC.Data_Race %>% filter(Year == 2010) %>%
-  ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = Mothers.Race)) + 
-  geom_point() + geom_line(aes(y = predict(avg_bw_mod2010)))
-MCH.CDC.Data_Race %>% filter(Year == 2007) %>%
-  ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = Mothers.Race)) + 
-  geom_point() + geom_line(aes(y = predict(avg_bw_mod2007)))
-MCH.CDC.Data_Race %>% filter(Year == 2019) %>%
-  ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = Mothers.Race)) + 
-  geom_point() + geom_line(aes(y = predict(avg_bw_mod2019)))
+  geom_point() + geom_smooth(se = F)
+
+df2 %>% mutate(agricultural = ifelse(County %in% agro, 1, 0)) %>%
+  filter(Year == 2019) %>%
+  ggplot(aes(Average.LMP.Gestational.Age, Average.Birth.Weight, color = factor(agricultural))) + 
+  geom_point()
+
+plot(avg_bw_mod2016)
+plot(mod1)
+
+plot(mod2)
+
+plot(mod3)
+
+
+
